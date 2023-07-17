aa9c3da0bfcb37f4f46ce16a1dbdbcf403ca155c
Elliot <ellthrowaway2@icloud.com>
2023-07-17 09:09:26 -0400

    SQL challenges progress

--- hackerrank_SQL_challenges.sql
+++ hackerrank_SQL_challenges.sql
@@ -1,8 +1,8 @@
-------- HackerRank's SQL 'Basic Select' -------
+------- HackerRank's SQL 'Basic Select' and 'Basic Join' -------
 
 ----------
 
--- Revising the Select Query I
+-- Basic Select -> Revising the Select Query I
 -- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 
 -- Solution:
@@ -10,7 +10,7 @@ SELECT * FROM city WHERE COUNTRYCODE = 'USA' AND population > 100000;
 
 ----------
 
--- Revising the Select Query II
+-- Basic Select -> Revising the Select Query II
 -- Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
 
 -- Solution:
@@ -18,7 +18,7 @@ SELECT name FROM city WHERE countrycode = 'USA' AND population > 120000;
 
 ----------
 
--- Select All
+-- Basic Select -> Select All
 -- Query all columns (attributes) for every row in the CITY table.
 
 -- Solution: 
@@ -26,7 +26,7 @@ SELECT * FROM city;
 
 ----------
 
--- Select By ID
+-- Basic Select -> Select By ID
 -- Query all columns for a city in CITY with the ID 1661.
 
 -- Solution: 
@@ -34,7 +34,7 @@ SELECT * FROM city WHERE id = 1661;
 
 ----------
 
--- Japanese Cities' Attributes
+-- Basic Select -> Japanese Cities' Attributes
 -- Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
 
 -- Solution: 
@@ -42,7 +42,7 @@ SELECT * FROM city WHERE countrycode = 'JPN';
 
 ----------
 
--- Japanese Cities' Names
+-- Basic Select -> Japanese Cities' Names
 -- Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
 
 -- Solution: 
@@ -50,7 +50,7 @@ SELECT name FROM city WHERE countrycode = 'JPN';
 
 ----------
 
--- Weather Observation Station 1
+-- Basic Select -> Weather Observation Station 1
 -- Query a list of CITY and STATE from the STATION table.
 
 -- Solution: 
@@ -58,7 +58,7 @@ SELECT city, state FROM station;
 
 ----------
 
--- Weather Observation Station 3
+-- Basic Select -> Weather Observation Station 3
 -- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
 
 -- Solution: 
@@ -66,7 +66,7 @@ SELECT DISTINCT city FROM station WHERE id % 2 = 0;
 
 ----------
 
--- Weather Observation Station 4
+-- Basic Select -> Weather Observation Station 4
 -- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 
 -- Solution: 
@@ -74,7 +74,7 @@ SELECT (COUNT(city) - COUNT(DISTINCT city)) FROM station;
 
 ----------
 
--- African Cities
+-- Basic Join -> African Cities
 -- Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
 -- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 
@@ -83,7 +83,7 @@ SELECT city.name FROM city JOIN country ON city.countrycode = country.code WHERE
 
 ----------
 
--- Average Population of Each Continent
+-- Basic Join -> Average Population of Each Continent
 -- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
 -- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 
@@ -92,15 +92,17 @@ SELECT country.continent, FLOOR(AVG(city.population)) FROM city JOIN country ON 
 
 ----------
 
--- 
--- 
+-- Basic Join -> Population Census
+-- Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 
 -- Solution: 
+SELECT SUM(city.population) FROM city JOIN country ON city.countrycode = country.code WHERE country.continent = 'Asia';
 
 ----------
 
--- 
--- 
+-- Basic Join -> The Report (med)
+-- You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
 
 -- Solution: 
 
