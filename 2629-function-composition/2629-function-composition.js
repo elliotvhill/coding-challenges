988ff8ad918d72e587f92f0815e989167d854234
Elliot Hill (he/him) <ellthrowaway2@icloud.com>
2024-04-04 12:03:34 -0700

    Time: 58 ms (84.67%), Space: 50.3 MB (31.27%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+const compose = function (functions) {
+    return function (x) {
+        if (functions.length === 0) {
+            return x
+        }
+        return functions.reduceRight((acc, fn) => fn(acc), x)
+    }
+}
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
