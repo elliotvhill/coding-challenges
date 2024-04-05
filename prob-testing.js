ee2c3e36ad598ffcec1641b42973b9031c325e10
Elliot <ellthrowaway2@icloud.com>
2024-04-05 12:17:58 -0500

    alt solution LC 2629

--- prob-testing.js
+++ prob-testing.js
@@ -13,6 +13,21 @@ const compose = function (functions) {
     }
 }
 
+// OR
+// const compose = function(functions) {
+//     return function(x) {
+//         if (functions.length === 0) return x
+//         let input = x
+
+//         for (let i = functions.length - 1; i >= 0; i--){
+//             const currentFunction = functions[i]
+//             input = currentFunction(input)
+//         }
+//         return input
+//     }
+// };
+
+
 const fn = compose([(x) => x + 1, (x) => 2 * x])
 //  fn(4) // returns 9
 
